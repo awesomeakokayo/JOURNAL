f9fe0abdda64b74ccfff671867dbbdc4fd6d6893
awesomeakokayo <awesomeakokayo@gmail.com>
2026-07-20 11:39:07 -0700

    switch from PrismaNeon WS to PrismaNeonHttp adapter

--- journal-platform/lib/prisma.ts
+++ journal-platform/lib/prisma.ts
@@ -1,20 +1,16 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaNeonHttp } from "@prisma/adapter-neon";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 function createPrismaClient() {
-  const adapter = new PrismaNeon({
-    connectionString: process.env.DATABASE_URL!,
-  });
-  return new PrismaClient({ adapter });
-}
-
-export function getPrisma() {
-  if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = createPrismaClient();
+  if (process.env.DATABASE_URL) {
+    const adapter = new PrismaNeonHttp(process.env.DATABASE_URL, {});
+    return new PrismaClient({ adapter });
   }
-  return globalForPrisma.prisma;
+  return new PrismaClient();
 }
 
-export const prisma = getPrisma();
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
